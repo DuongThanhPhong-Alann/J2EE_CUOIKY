55248ea758bfd022c301966a2c2e3a88d16bb5e2
DuongThanhPhong-Alann <duongthanhphong1618@gmail.com>
2026-03-31 07:57:12 +0700

    J2EE_1

---
+++ src/main/java/APT/J2EE/J2EeApplication.java
@@ -0,0 +1,13 @@
+package APT.J2EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2EeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2EeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/APT/J2EE/J2EeApplicationTests.java
@@ -0,0 +1,13 @@
+package APT.J2EE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2EeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
